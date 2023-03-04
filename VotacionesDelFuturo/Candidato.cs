b1b363e10069a008d047871fbc87634881879a63
cvazq <cvazq@DESKTOP-8DFFOC5>
2023-03-04 09:33:04 -0600

    Agregar archivos de proyecto.

---
+++ VotacionesDelFuturo/Candidato.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VotacionesDelFuturo
+{
+    internal class Candidato
+    {
+        public int Indice { get; set; }
+        public int Votos { get; set; }
+    }
+}
---
+++ VotacionesDelFuturo/Votacion.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace VotacionesDelFuturo
+{
+    internal class Votacion
+    {
+        private List<Candidato> Candidatos= new List<Candidato>();
+        private int Votantes;
+
+        public void InicializarVotacion(int noCandidatos, int noVotantes)
+        {
+            GenerarCandidatos(noCandidatos);
+            Votantes = noVotantes;
+        }
+
+        private void GenerarCandidatos(int noCandidatos)
+        {
+            for (int i = 0; i < noCandidatos; i++)
+            {
+                Candidatos.Add(new Candidato() { Indice = i });
+            }
+        }
+
+        public bool ValidarCandidato(int indiceCandidato)
+        {
+            return Candidatos.Where(x => x.Indice == indiceCandidato).Any();
+        }
+
+        public void EmitirVoto(int indiceCandidato)
+        {
+            var candidato = Candidatos.Find(x => x.Indice == indiceCandidato);
+            candidato.Votos++;
+        }
+
+        public bool VotacionCompleta()
+        {
+            return Candidatos.Select(x => x.Votos).Sum() == Votantes;
+        }
+
+        public int getIndiceUltimoCandidato()
+        {
+            return Candidatos.Count() - 1;
+        }
+
+        public string MostrarResultados()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Resultados!");
+            sb.AppendLine();
+            foreach (var candidato in Candidatos.OrderByDescending(x => x.Votos).ToList())
+            {
+                sb.AppendLine($"Candidato: {candidato.Indice}, Total de Votos:  {candidato.Votos}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
